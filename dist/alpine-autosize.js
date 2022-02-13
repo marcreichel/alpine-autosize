41b3da95e3c90b2756818c1cf90dbfabd91a1d4a
Marc Reichel <marc.reichel@artemeon.de>
2022-02-13 13:03:20 +0100

    ✨ Finalize autoresize feature

--- dist/alpine-autosize.js
+++ dist/alpine-autosize.js
@@ -4,9 +4,26 @@
 })((function () { 'use strict';
 
     function Autosize(Alpine) {
-      Alpine.directive('autosize', el => {
-        el.style.height = '4px';
-        el.style.height = `${el.scrollHeight}px`;
+      Alpine.directive('autosize', (el, {}, {
+        cleanup
+      }) => {
+        const previousResizeValue = el.style.resize;
+        el.style.resize = 'none';
+
+        const handler = event => {
+          const element = event.target;
+          element.style.height = '4px';
+          element.style.height = `${element.scrollHeight}px`;
+        };
+
+        handler({
+          target: el
+        });
+        el.addEventListener('input', handler);
+        cleanup(() => {
+          el.style.resize = previousResizeValue;
+          el.removeEventListener('input', handler);
+        });
       });
     }
 
