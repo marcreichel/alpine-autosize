44540cf8e708a041bfb591ddaf36a6458e0ec29a
Felix Wittmann <jfwittmann7@googlemail.com>
2022-03-27 13:58:54 +0200

    🐛 Fix `wire:model` behavior
    * fix #1, problem with wire:model
    * ⚡️ Check if `wire:ignore` is already set
    
    Co-authored-by: jfwittmann <jfwittmann7@gmail.com>
    Co-authored-by: Marc Reichel <mail@marcreichel.de>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 function Autosize(Alpine) {
     Alpine.directive('autosize', (el, {}, { cleanup }) => {
+        
+        if (!el.hasAttribute('wire:ignore') && el.hasAttribute('wire:model')) {
+            el.setAttribute('wire:ignore', '');
+        }
+
         const previousResizeValue = el.style.resize;
         el.style.resize = 'none';
 
         const previousMinHeight = el.style.minHeight;
         el.style.minHeight = el.getBoundingClientRect().height + 'px';
-
         const handler = (event) => {
             const element = event.target;
             element.style.height = '4px';
